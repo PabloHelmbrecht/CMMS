c39f0605fade3ebc3de701d64fa0d46276139ab6
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2022-07-27 20:42:19 -0300

    ThemeCustomization Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Routes from 'routes';
-import ThemeCustomization from 'themes';
-import ScrollTop from 'components/ScrollTop';
+//import Routes from 'routes';
+import ThemeCustomization from './themes';
+//import ScrollTop from 'components/ScrollTop';
 
 
 function App() {
   return (
     <ThemeCustomization>
-      <h1></h1>
+      <h1>Holaa</h1>
     </ThemeCustomization>
   );
 }
