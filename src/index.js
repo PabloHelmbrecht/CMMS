d4757d7cde12fde58f96d95e88ae272dec51eaff
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2022-07-27 00:17:37 -0300

    Added redux provider

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider as ReduxProvider } from "react-redux";
 
 // project import
 import App from "./App";
-import { store } from "store";
+import { store } from "./store";
 import reportWebVitals from "./reportWebVitals";
 import { sendToVercelAnalytics } from "./vitals";
 
