36eebabbecfb1c418081c5af479e30e83786ce20
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2022-07-29 20:15:56 -0300

    bugs in mainlayout fixed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 //import repositories
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { lazy } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 // import themes
 import ThemeCustomization from "./themes";
 //import ScrollTop from "#components/ScrollTop";
-
-//project render imports
-import Loadable from "#components/Loadable";
-import MainLayout from "#layout/MainLayout";
-
-//pages render
-const DashboardDefault = Loadable(lazy(() => import("#pages/dashboard")));
-const SamplePage = Loadable(
-  lazy(() => import("#pages/extra-pages/SamplePage"))
-);
-const Typography = Loadable(
-  lazy(() => import("#pages/components-overview/Typography"))
-);
-const Color = Loadable(lazy(() => import("#pages/components-overview/Color")));
-const Shadow = Loadable(
-  lazy(() => import("#pages/components-overview/Shadow"))
-);
-const AntIcons = Loadable(
-  lazy(() => import("#pages/components-overview/AntIcons"))
-);
+import Routes from "./routes"
 
 function App() {
   return (
     <ThemeCustomization>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DashboardDefault />}/>
-
-        </Routes>
+        <Routes/>
       </BrowserRouter>
     </ThemeCustomization>
   );
