a351fada9a45f97b9d517b21d3505becc0998b56
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2022-07-29 19:31:24 -0300

    ui generated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-//import Routes from 'routes';
+//import repositories
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+
+// import themes
 import ThemeCustomization from "./themes";
-//import ScrollTop from "./components/ScrollTop";
+//import ScrollTop from "#components/ScrollTop";
+
+//project render imports
+import Loadable from "#components/Loadable";
+import MainLayout from "#layout/MainLayout";
+
+//pages render
+const DashboardDefault = Loadable(lazy(() => import("#pages/dashboard")));
+const SamplePage = Loadable(
+  lazy(() => import("#pages/extra-pages/SamplePage"))
+);
+const Typography = Loadable(
+  lazy(() => import("#pages/components-overview/Typography"))
+);
+const Color = Loadable(lazy(() => import("#pages/components-overview/Color")));
+const Shadow = Loadable(
+  lazy(() => import("#pages/components-overview/Shadow"))
+);
+const AntIcons = Loadable(
+  lazy(() => import("#pages/components-overview/AntIcons"))
+);
 
 function App() {
   return (
     <ThemeCustomization>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashboardDefault />}/>
 
-        <h1>Holaa</h1>
-
+        </Routes>
+      </BrowserRouter>
     </ThemeCustomization>
   );
 }
