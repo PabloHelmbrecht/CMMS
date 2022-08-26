787eef10943463bb9f6542a5bdf271337dc9f219
Pablo <pablohelmbrecht@gmail.com>
2022-08-26 18:55:38 -0300

    Vista de equipos mejorada

--- src/pages/elementos/equipos/VistaEquipo.jsx
+++ src/pages/elementos/equipos/VistaEquipo.jsx
@@ -2,12 +2,33 @@
 import React, { useEffect, useState } from "react";
 import {
     Button,
-    Dialog
+    Dialog,
+    Divider,
+    Typography,
+    Card,
+    Grid,
+    IconButton,
+    Stack,
+    Chip
 } from "@mui/material";
 import { Link, useLocation, useParams } from "react-router-dom";
 
-//Project imports
-import MainCard from "../../../components/MainCard";
+//icons imports
+import {
+    CloseOutlined,
+    EyeFilled,
+    EditFilled,
+    CaretDownOutlined,
+    PrinterFilled,
+    LinkOutlined
+} from '@ant-design/icons';
+
+
+/*Función para copiar link no est{a listo todavía*/
+const Clipboard = async () => {
+    await navigator.clipboard.writeText(useLocation().pathname);
+}
+
 
 
 
@@ -16,7 +37,7 @@ const VistaEquipo = () => {
     //Obtenemos el id de la ruta
     const { id, mode } = useParams()
     const location = useLocation()
-    
+
     //Declaramos los estados
     const [open, setOpen] = useState(Number.isInteger(parseInt(id)) && parseInt(id) > 0)
     const [editMode, setEditMode] = useState(mode === "edit")
@@ -41,25 +62,116 @@ const VistaEquipo = () => {
         <Dialog open={open}
             fullWidth={true}
             maxWidth={'xl'}
-            PaperProps={{
-                style: { backgroundColor: 'transparent', }
-            }}
+            sx={{ height: '100%' }}
         >
-            <MainCard title="Primary Color"  >
-                id: {id} view is open: {open ? "true" : "false"} editMode: {editMode ? "true" : "false"}
-                <Button component={Link} to={`/elementos/equipos`}>
-                    Cerrar
-                </Button>
-                {editMode ?
-                    <Button component={Link} to={`/elementos/equipos/${id} `}>
-                        Cambiar a modo vista
-                    </Button>
-                    :
-                    <Button component={Link} to={`/elementos/equipos/${id}/edit `} >
-                        Cambiar a modo edición
-                    </Button>
-                }
-            </MainCard>
+
+            {/*Header de la vista*/}
+            <Grid
+                container
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{ p: 2 }}
+            >
+
+                <Grid item sx={6} >
+                    <Stack
+                        direction="row"
+                        justifyContent="flex-end"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        <Chip label="Equipo" color="primary" icon={<CaretDownOutlined />} />
+                    </Stack>
+                </Grid>
+
+                <Grid item sx={6}>
+                    <Stack
+                        direction="row"
+                        justifyContent="flex-end"
+                        alignItems="center"
+                        spacing={2}
+                    >
+                        {
+                            editMode ?
+                                <Button
+                                    component={Link}
+                                    to={`/elementos/equipos/${id}`}
+                                    size="small"
+                                    variant="contained"
+                                    color="primary"
+                                    startIcon={<EyeFilled />}>
+                                    modo vista
+                                </Button>
+                                :
+                                <Button
+                                    component={Link}
+                                    to={`/elementos/equipos/${id}/edit`}
+                                    size="small"
+                                    variant="contained"
+                                    color="primary"
+                                    startIcon={<EditFilled />}>
+                                    modo edición
+                                </Button>
+                        }
+                        <IconButton component={Link} to={`/elementos/equipos`}>
+                            <CloseOutlined />
+                        </IconButton>
+                    </Stack>
+                </Grid>
+            </Grid>
+
+            {/*Divisor de la vista*/}
+            <Divider />
+
+            {/*Body de la vista*/}
+            <Grid
+                container
+                spacing={2}
+                sx={{ p: 2 }}
+            >
+
+                {/*Datos de carga del elemento*/}
+                <Grid item xs={12} sm={12} md={8} lg={8}>
+                    <Stack
+                        direction="column"
+                        spacing={2}
+                    >
+                        <Card variant="outlined">card</Card>
+                        <Card variant="outlined">card</Card>
+                        <Card variant="outlined">card</Card>
+                    </Stack>
+                </Grid>
+
+                {/*Actividades del la vista*/}
+                <Grid item xs={12} sm={12} md={4} lg={4}>
+                    <Card variant="outlined" sx={{ height: 1 }}>card</Card>
+
+                </Grid>
+
+            </Grid>
+
+            {/*Divisor de la vista*/}
+            <Divider />
+
+            {/*Footer de la vista*/}
+
+            <Stack
+                direction="row"
+                justifyContent="flex-end"
+                spacing={1}
+                sx={{
+                    p: 1,
+                    pl: 2,
+                    pr: 2
+                }}
+            >
+                <IconButton onClick={Clipboard()} color="secondary"  >
+                    <LinkOutlined />
+                </IconButton>
+                <IconButton component={Link} to={`/elementos/equipos`} color="secondary" >
+                    <PrinterFilled />
+                </IconButton>
+            </Stack>
         </Dialog>
     )
 }
